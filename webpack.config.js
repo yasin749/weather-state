58d38595d7bc13310248642f90c65f40664b93b1
Yasin UYSAL <yasin.uysal@mynetgroup.com>
2018-05-28 01:36:36 +0300

    webpack css compile config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 var path = require('path');
 var webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: './resources/assets/App.js',
+    entry: ['./resources/assets/App.js','./resources/assets/sass/style.scss'],
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'app.js'
@@ -15,8 +17,37 @@ module.exports = {
                     presets: ['es2015']
                 }
             }
+        ],
+        rules: [
+            {
+                test: /\.(css|sass|scss)$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false,
+                                minimize: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ],
+                })
+            }
         ]
     },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'style.css',
+            allChunks: true,
+        }),
+    ],
     stats: {
         colors: true
     },
