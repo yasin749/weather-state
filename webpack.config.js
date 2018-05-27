1f0229df3cba558b6187d8475f7488d5a4ee1d3b
Yasin UYSAL <yasin.uysal@mynetgroup.com>
2018-05-28 02:23:49 +0300

    Node sass setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: ['./resources/assets/App.js','./resources/assets/sass/style.scss'],
+    entry: ['./resources/assets/App.js'],
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'app.js'
@@ -18,41 +17,14 @@ module.exports = {
                 }
             }
         ],
-        rules: [
-            {
-                test: /\.(css|sass|scss)$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                url: false,
-                                minimize: true,
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }
-                    ],
-                })
-            }
-        ]
+
     },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'style.css',
-            allChunks: true,
-        }),
-    ],
     stats: {
         colors: true
     },
     devtool: 'source-map'
 };
+
 
 if (process.env.NODE_ENV === 'production') {
     module.exports.devtool = '#source-map'
