6ab10a1bf298f2610a6e43de2b5650b0b7c63955
Yasin UYSAL <yasin.uysal@mynetgroup.com>
2018-05-28 02:43:52 +0300

    fix

--- resources/assets/js/components/weather-box.js
+++ resources/assets/js/components/weather-box.js
@@ -31,7 +31,6 @@ var weatherBox = Vue.component('weather-box', {
                 <img v-bind:src="imageUrlPrepared()" class="ico" v-bind:alt="city.name">
                 <span class="temperature">{{this.city.main.temp}} °С</span>
                 <span class="state">{{this.city.weather[0].main}}</span>
-                <span class="description">{{this.city.weather[0].description}}</span>
                 <span class="wind">Wind : {{this.city.wind.speed}} m/s</span>
                 <span class="button small border-button">Five day forecast</span>
             </div>
