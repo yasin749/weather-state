3fc6390454100f40559fd8aa35dbffc924fa35d4
Yasin UYSAL <yasin.uysal@mynetgroup.com>
2018-07-13 00:47:42 +0300

    Container

--- react/resources/assets/js/components/weather-box-list.js
+++ react/resources/assets/js/components/weather-box-list.js
@@ -63,10 +63,10 @@ class WeatherBoxList extends React.Component {
         const {error, isLoaded, cities} = this.state;
 
         if (error) {
-            return <div>Error: {error.message}</div>;
+            return <div className="container">Error: {error.message}</div>;
         }
         else if (!isLoaded) {
-            return <div>Loading...</div>;
+            return <div className="container">Loading...</div>;
         }
         else {
             return (
